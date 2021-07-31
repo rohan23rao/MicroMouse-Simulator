ebe981c1a2f5e30dcbac5eb5bf9121eb3e629f98
Rohan Rao <rohan23rao@gmail.com>
2021-07-31 16:24:40 -0700

    added cardinal directions

--- microMouseServer/microMouseServer/studentai.cpp
+++ microMouseServer/microMouseServer/studentai.cpp
@@ -1,5 +1,64 @@
-
+#include <iostream>
 #include "micromouseserver.h"
+
+void turn_store(int (&position)[20][20], int &current_facing, int moving_direction, int x, int y) {
+    switch (current_facing) {
+        case 0:
+            switch (moving_direction) {
+                case 0:
+                    y++;
+                    break;
+                case 1:
+                    current_facing = 1;
+                    break;
+                case 2:
+                    current_facing = 3;
+                    break;
+            }
+            break;
+        case 1:
+            switch (moving_direction) {
+                case 0:
+                    x++;
+                    break;
+                case 1:
+                    current_facing = 2;
+                    break;
+                case 2:
+                    current_facing = 0;
+                    break;
+            }
+            break;
+        case 2:
+            switch (moving_direction) {
+                case 0:
+                    y--;
+                    break;
+                case 1:
+                    current_facing = 3;
+                    break;
+                case 2:
+                    current_facing = 1;
+                    break;
+            }
+            break;
+        case 3:
+            switch (moving_direction) {
+                case 0:
+                    x--;
+                    break;
+                case 1:
+                    current_facing = 0;
+                    break;
+                case 2:
+                    current_facing = 2;
+                    break;
+            }
+            break;
+    }
+
+}
+
 
 void microMouseServer::studentAI()
 {
@@ -21,5 +80,37 @@ void microMouseServer::studentAI()
  * void foundFinish();
  * void printUI(const char *mesg);
 */
+    static int position[20][20] = {{0}};
+    static int ending = 0;
+    position[0][0] = 0;
+    static int x = 0, y = 0;
+    static int current_facing = 0; // 0 is facing north, 1 is facing east, 2 is facing south, 3 is facing west
+//    static int moving_direction = 0;  // 0 is move forward, 1 is turn right, 2 is turn left
 
+    if (!isWallLeft()) {
+//    turnLeft();
+//    moveForward();
+    turn_store(position, current_facing, 2, x, y);
+    turn_store(position, current_facing, 0, x, y);
+    ending = 0;
+    }
+    else if (!isWallForward()) {
+    moveForward();
+    ending = 0;
+    }
+    else if (!isWallRight()) {
+    turnRight();
+    moveForward();
+    ending++;
+    }
+    else { // turn around
+    turnRight();
+    turnRight();
+    moveForward();
+    ending = 0;
+    }
+
+    if (ending == 3) {
+    foundFinish();
+    }
 }
