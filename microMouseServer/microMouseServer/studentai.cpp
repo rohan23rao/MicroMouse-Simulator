9ed87abf72e722ad8a20b7adb3cbff8dee921994
Rohan Rao <rohan23rao@gmail.com>
2021-08-02 21:45:52 -0700

    Added turn counters, position updating and movement correction

--- microMouseServer/microMouseServer/studentai.cpp
+++ microMouseServer/microMouseServer/studentai.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
+using namespace std;
 #include "micromouseserver.h"
+//void turn_store(int (&position)[20][20], int &current_facing, int moving_direction, int x, int y);
 
-void turn_store(int (&position)[20][20], int &current_facing, int moving_direction, int x, int y) {
+int *turn_store(int &current_facing, int moving_direction, int x, int y) {
     switch (current_facing) {
         case 0:
             switch (moving_direction) {
@@ -56,9 +58,43 @@ void turn_store(int (&position)[20][20], int &current_facing, int moving_directi
             }
             break;
     }
+    static int xy[2] = {x, y};
+    return xy;
 
 }
 
+
+int timesLeft_(int (&position)[20][20], int current_facing, int x, int y, bool is_left) {
+    if (is_left) {
+        return INT_MAX;
+    }
+    current_facing = ((current_facing+3) % 4);
+    int *ptr = turn_store(current_facing, 0, x, y);
+    return position[ptr[0]][ptr[1]];
+}
+
+bool timesForward_(int (&position)[20][20], int current_facing, int x, int y, bool is_forward) {
+    if (is_forward) {
+        return INT_MAX;
+    }
+    current_facing = ((current_facing+1) % 4);
+    int *ptr = turn_store(current_facing, 0, x, y);
+    return position[ptr[0]][ptr[1]];
+}
+
+bool timesRight_(int (&position)[20][20], int current_facing, int x, int y, bool is_right) {
+    if (is_right) {
+        return INT_MAX;
+    }
+    current_facing = ((current_facing+2) % 4);
+    int *ptr = turn_store(current_facing, 0, x, y);
+    return position[ptr[0]][ptr[1]];
+}
+
+void update(int (&position)[20][20], int current_facing, int moving_direction, int x, int y) {
+    int *ptr = turn_store(current_facing, moving_direction, x, y);
+    position[ptr[0]][ptr[1]]++;
+}
 
 void microMouseServer::studentAI()
 {
@@ -84,24 +120,39 @@ void microMouseServer::studentAI()
     static int ending = 0;
     position[0][0] = 0;
     static int x = 0, y = 0;
+
     static int current_facing = 0; // 0 is facing north, 1 is facing east, 2 is facing south, 3 is facing west
 //    static int moving_direction = 0;  // 0 is move forward, 1 is turn right, 2 is turn left
-
-    if (!isWallLeft()) {
+//    bool wall_locations[3] = {isWallLeft(), isWallRight(), isWallForward()};
+    bool timesLeft = timesLeft_(position, current_facing, x, y, isWallLeft());
+    bool timesRight = timesRight_(position, current_facing, x, y, isWallRight());
+    bool timesForward = timesForward_(position, current_facing, x, y, isWallForward());
+    cout << "Times Left: " << timesLeft << endl;
+    cout << "Times Right: " << timesRight << endl;
+    cout << "Times Forward: " << timesForward << endl;
+    if (!isWallLeft() && timesLeft<=timesForward && timesForward<=timesRight) {
 //    turnLeft();
 //    moveForward();
-    turn_store(position, current_facing, 2, x, y);
-    turn_store(position, current_facing, 0, x, y);
-    ending = 0;
+        update(current_facing, 2, x, y);
+        turnLeft();
+        update(current_facing, 0, x, y);
+        moveForward();
+
+        ending = 0;
     }
-    else if (!isWallForward()) {
-    moveForward();
-    ending = 0;
+
+    else if (!isWallForward() && timesForward<=timesRight) {
+        update(current_facing, 0, x, y);
+        moveForward();
+        ending = 0;
     }
+
     else if (!isWallRight()) {
-    turnRight();
-    moveForward();
-    ending++;
+        update(current_facing, 1, x, y);
+        turnRight();
+        update(current_facing, 0, x, y);
+        moveForward();
+        ending++;
     }
     else { // turn around
     turnRight();
